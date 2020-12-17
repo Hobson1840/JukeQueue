b606d2e9f3af7ec5eb218ba6d53a50c91994a4e5
Matthew Hobson <matthew.hobson174@aggiemail.usu.edu>
2020-12-17 11:16:08 -0700

    added plan info

--- js/test.js
+++ js/test.js
@@ -2,18 +2,18 @@ let watches = {
   products: [
     {
       name: "Freemium",
-      description: "Free info",
+      description: "Plan info <ul><li>Free voting</li> <li>$1 per song</li></ul> ",
       img: "photo-1569926426428-471bfd09906a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=933&q=80",
     },
     {
       name: "JukeboxHero",
-      description: "Hero info",
+      description: "Plan info <ul><li>Priority voting</li><li>$5 a month</li><li>$50 a year</li></ul>",
       img: "photo-1550871645-95ffeae86cb8?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=934&q=80",
     },
     {
       name: "Vendor",
       description:
-        "Vendor info",
+        " Plan Info <ul> <li>Now free to try in Logan, UT</li><li>Pricing varies on location</li></ul> ",
       img: "photo-1548696060-8fae845c6452?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=934&q=80",
     }
   ],
